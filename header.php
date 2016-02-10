58908bf5f90c3d1c7f29b6df2c5b06abcfd34ba8
Chloe Weber <chloeesierraa@yahoo.com>
2016-02-10 14:59:22 -0500

    Populate header file with header code

--- header.php
+++ header.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+<?php wp_head(); ?>
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('s
+tylesheet_url'); ?>" />
+    </head>
 
+    <body>
+
+        <div class="container">
+
+            <header class="row">
+                <div class="twelve columns">
+                    <h1><?php bloginfo('name'); ?></h1>
+                    <p><?php bloginfo('description'); ?></p>
+                </div>
+            </header>
+<!-- END OF HEADER - BEGIN SECTION CONTENT -->
