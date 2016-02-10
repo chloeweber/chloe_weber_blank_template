642d2027874447f2743c3f5e0c3ec09bcc71d7c9
Chloe Weber <chloeesierraa@yahoo.com>
2016-02-10 16:40:06 -0500

    Add widgets code to footer file

--- footer.php
+++ footer.php
@@ -1,11 +1,15 @@
-<!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
             <footer class="row">
-                <div class="twelve columns">
-                    <p>Footer content goes here.</p>
+                <div class="four columns">
+                    <?php dynamic_sidebar('footer-widget-one'); ?>
+                </div>
+                <div class="four columns">
+                    <?php dynamic_sidebar('footer-widget-two'); ?>
+                </div>
+                <div class="four columns">
+                    <?php dynamic_sidebar('footer-widget-three'); ?>
                 </div>
             </footer>
-        </div> <!-- ends container -->
-<?php wp_footer(); ?>
+        </div>
+        <?php wp_footer(); ?>
     </body>
-
 </html>
