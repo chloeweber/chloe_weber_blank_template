a8a8306109026d0e069ed78298a29f8b307c9496
Chloe Weber <chloeesierraa@yahoo.com>
2016-02-11 00:30:02 -0500

    Add footer widgets

--- functions.php
+++ functions.php
@@ -12,6 +12,36 @@ function blank_widgets_init() {
         'before_title' => '<h2>',
         'after_title' => '</h2>'
         ));
+    /*--- New Widget --- */
+    register_sidebar( array(
+        'name' => ('Footer Widget Left'),
+        'id' => 'footer-widget-one',
+        'description' => 'First widget for our footer',
+        'before_widget' => '<div class="widget-footer">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+        ));
+    /*--- Second New Widget --- */
+    register_sidebar( array(
+        'name' => ('Footer Widget Middle'),
+        'id' => 'footer-widget-two',
+        'description' => 'Second widget for our footer',
+        'before_widget' => '<div class="widget-footer">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+        ));
+    /*--- Third New Widget --- */
+    register_sidebar( array(
+        'name' => ('Footer Widget Right'),
+        'id' => 'footer-widget-three',
+        'description' => 'Third widget for our footer',
+        'before_widget' => '<div class="widget-footer">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+        ));
 }
 add_action('widgets_init', 'blank_widgets_init');
 
