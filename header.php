81398e1267b9113225bad5497eaf1bea9f983d48
Chloe Weber <chloeesierraa@yahoo.com>
2016-03-20 17:08:46 -0500

    Add logo and change header html

--- header.php
+++ header.php
@@ -9,22 +9,21 @@
     <body>
         <div class="container">
             <header class="row">
-                <div class="nine columns">
-                    <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-                    <h2><?php bloginfo('description'); ?></h2>
+                <div class="twelve columns">
+                    <h1 class="visuallyhidden">Chloe Weber Blog</h1>
+                    <a href="<?php bloginfo('url'); ?>"><img src="wp-content/themes/chloe_weber_blank_template/img/logo.png" alt="Logo" /></a>
+                    <p id="header-description"><?php bloginfo('description'); ?></p>
                 </div>
-                <div class="three columns">
-                    <?php get_search_form(); ?>
+<!-- Add Menu Here -->
+                <div class="row">
+                    <div class="twelve columns">
+                        <?php wp_nav_menu(array(
+                            'menu' => 'Nav Menu',
+                            'sort_column' => 'menu_order',
+                            'container_class' => 'blank-menu-header'
+                        ));?>
+                    </div>
                 </div>
             </header>
+
 <!-- END OF HEADER - BEGIN SECTION CONTENT -->
-<!-- Add Menu Here -->
-            <div class="row">
-                <div class="twelve columns">
-                    <?php wp_nav_menu(array(
-                        'menu' => 'Main Menu',
-                        'sort_column' => 'menu_order',
-                        'container_class' => 'blank-menu-header'
-                    ));?>
-                </div>
-            </div>
