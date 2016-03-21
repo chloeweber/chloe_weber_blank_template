8063ed1e12b216008d69355defa877b7b4787f02
Chloe Weber <chloeesierraa@yahoo.com>
2016-03-21 16:27:18 -0500

    Add final single file

--- single.php
+++ single.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-
     <section class="row">
         <div class="two columns">
             &nbsp;
@@ -12,9 +11,12 @@
                 while ( have_posts() ) {
                     the_post();
                     if ( has_post_thumbnail() ) { ?>
-                        <div class="post-thumbnail"><?php the_post_thumbnail('large'); ?></div>
+                        <div class="single-thumbnail"><?php the_post_thumbnail('large'); ?></div>
                     <?php } ?>
                     <h3><?php the_title(); ?></h3>
+                    <div class="date-author-single">
+                        <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?> </small>
+                    </div>
                     <?php the_content();
                 } // end while
             } // end if
