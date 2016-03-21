0e39887c540e812bdb99f8bf6f462e816ce95bac
Chloe Weber <chloeesierraa@yahoo.com>
2016-03-21 16:24:42 -0500

    Add final functions file

--- functions.php
+++ functions.php
@@ -4,40 +4,10 @@
 
 function blank_widgets_init() {
     register_sidebar( array(
-        'name' => ('First Widget'),
-        'id' => 'first-widget',
-        'description' => 'Widget for our sidebar on pages',
-        'before_widget' => '<div class="widget-sidebar">',
-        'after_widget' => '</div>',
-        'before_title' => '<h2>',
-        'after_title' => '</h2>'
-        ));
-    /*--- New Widget --- */
-    register_sidebar( array(
-        'name' => ('Footer Widget Left'),
-        'id' => 'footer-widget-left',
-        'description' => 'First widget for our footer',
-        'before_widget' => '<div class="widget-footer">',
-        'after_widget' => '</div>',
-        'before_title' => '<h2>',
-        'after_title' => '</h2>'
-        ));
-    /*--- Second New Widget --- */
-    register_sidebar( array(
-        'name' => ('Footer Widget Middle'),
-        'id' => 'footer-widget-middle',
-        'description' => 'Second widget for our footer',
-        'before_widget' => '<div class="widget-footer">',
-        'after_widget' => '</div>',
-        'before_title' => '<h2>',
-        'after_title' => '</h2>'
-        ));
-    /*--- Third New Widget --- */
-    register_sidebar( array(
-        'name' => ('Footer Widget Right'),
-        'id' => 'footer-widget-right',
-        'description' => 'Third widget for our footer',
-        'before_widget' => '<div class="widget-footer">',
+        'name' => ('Social Widget'),
+        'id' => 'social-widget',
+        'description' => 'Widget for social media header',
+        'before_widget' => '<div class="widget-header">',
         'after_widget' => '</div>',
         'before_title' => '<h2>',
         'after_title' => '</h2>'
