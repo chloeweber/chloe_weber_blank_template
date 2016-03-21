b86f9694ac3db0c237d73769dcb495fc1a4402ab
Chloe Weber <chloeesierraa@yahoo.com>
2016-03-21 16:25:08 -0500

    Add final header file

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title><?php bloginfo('description'); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
         <link href='https://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css' />
 <?php wp_head(); ?>
@@ -15,14 +16,18 @@
                     <a href="<?php bloginfo('url'); ?>"><img src="wp-content/themes/chloe_weber_blank_template/img/logo.png" alt="Logo" id="logo"/></a>
                     <p id="header-description"><?php bloginfo('description'); ?></p>
                 </div>
-<!-- Add Menu Here -->
+<!-- Menu -->
                 <div class="row">
-                    <div class="twelve columns">
+                    <div class="eight columns">
                         <?php wp_nav_menu(array(
                             'menu' => 'Nav Menu',
                             'sort_column' => 'menu_order',
                             'container_class' => 'blank-menu-header'
                         ));?>
+                    </div>
+
+                    <div class="four columns">
+                        <?php dynamic_sidebar('social-widget'); ?>
                     </div>
                 </div>
             </header>
